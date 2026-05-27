f43541e5d7a2e9f82cc8b370f02dc060de61b46e
AllenCodess <Aph296@protonmail.com>
2026-05-27 19:14:53 -0400

    added logo, links, icons to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import { Link } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
     <>
       <div className="container">
-        <div className="flexed-logo">
-          <h1 className="logo-name">Huntley's</h1>
-          <h4 className="secondary-name">SAUCES</h4>
-          <Link to="/products">SHOP ALL</Link>
-          <Link to="/about">ABOUT US</Link>
-          <Link to="contact">CONTACT</Link>
+        <div className="flexed-navigation">
+          <div className="navigation-logo">
+            <h1 className="logo-name">Huntley's</h1>
+            <h4 className="secondary-name">SAUCES</h4>
+          </div>
+          <div className="navigation-links">
+            <Link className="nav-links" to="/products">
+              SHOP ALL
+            </Link>
+            <Link className="nav-links" to="/about">
+              ABOUT US
+            </Link>
+            <Link className="nav-links" to="/contact">
+              CONTACT
+            </Link>
+          </div>
+          <div className="navigation-icons">
+            <Link to="/signin">
+              <FontAwesomeIcon className="nav-icon" icon={faUser} />
+            </Link>
+            <Link to="/cart">
+              <FontAwesomeIcon className="nav-icon" icon={faCartShopping} />
+            </Link>
+          </div>
         </div>
       </div>
     </>
