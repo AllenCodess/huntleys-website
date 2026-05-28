be3f6cd40f4dee2fb4f0807192262f7f5d2410c3
AllenCodess <Aph296@protonmail.com>
2026-05-27 23:49:48 -0400

    I didnt add the class properly

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -5,11 +5,18 @@ const HomeScreen = () => {
     <>
       <Header />
       <div className="hero-container container">
-        <img
-          className="hero-img"
-          src="/public/images/LandingPage-Images/hero-section-img.png"
-          alt=""
-        />
+        <div className="img-container">
+          <img
+            className="hero-img"
+            src="/public/images/LandingPage-Images/hero-section-img.png"
+            alt=""
+          />
+          <div className="hero-text-container">
+            <h3>Premium Sauces & Dips</h3>
+            <h1>BOLD FLAVOUR.</h1>
+            <h1>MADE FOR MORE.</h1>
+          </div>
+        </div>
       </div>
     </>
   );
