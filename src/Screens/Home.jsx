e4a8e65cd9c89a770d48650790be0c83da0dff31
AllenCodess <Aph296@protonmail.com>
2026-05-28 17:03:40 -0400

    added text inside image, got rid of image container and added fontawesome icon with border

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,11 +1,31 @@
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
 
 const HomeScreen = () => {
   return (
     <>
       <Header />
       <HeroSection />
+      <section className="container section-text">
+        <div className="section-flex-container">
+          <div className="section-flex-item">
+            <div className="section-text-container">
+              <FontAwesomeIcon className="circle-icon" icon={faStar} size="lg" />
+              <h2 className="section-header">SAUCES</h2>
+              <p className="section-description">Bold flavours for every occasion.</p>
+              <button>SHOP SAUCES</button>
+            </div>
+
+            <img
+              className="section-img"
+              src="../public/images/LandingPage-Images/sauce.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
