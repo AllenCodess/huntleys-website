d29028e2b1051ebfb6f6ef43e271aee8539c22a8
AllenCodess <Aph296@protonmail.com>
2026-05-28 17:34:11 -0400

    flexed items for the sauces, sign-in, and featured section

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-regular-svg-icons";
+import { faUser, faStar } from "@fortawesome/free-regular-svg-icons";
 
 const HomeScreen = () => {
   return (
@@ -12,15 +12,45 @@ const HomeScreen = () => {
         <div className="section-flex-container">
           <div className="section-flex-item">
             <div className="section-text-container">
-              <FontAwesomeIcon className="circle-icon" icon={faStar} size="lg" />
+              <FontAwesomeIcon className=" circle-icon" icon={faStar} size="lg" />
               <h2 className="section-header">SAUCES</h2>
               <p className="section-description">Bold flavours for every occasion.</p>
-              <button>SHOP SAUCES</button>
+              <button className="hero-btn">SHOP SAUCES</button>
             </div>
 
             <img
               className="section-img"
               src="../public/images/LandingPage-Images/sauce.png"
+              alt=""
+            />
+          </div>
+          <div className="section-flex-item">
+            <div className="section-text-container">
+              <FontAwesomeIcon className=" circle-icon" icon={faStar} size="lg" />
+              <h2 className="section-header">SIGN IN</h2>
+              <p className="section-description">
+                Sign in to your account to track orders, save favorites, and more.
+              </p>
+              <button className="hero-btn">SIGN IN</button>
+            </div>
+
+            <img
+              className="section-img"
+              src="../public/images/LandingPage-Images/signin-img.png"
+              alt=""
+            />
+          </div>
+          <div className="section-flex-item">
+            <div className="section-text-container">
+              <FontAwesomeIcon className=" circle-icon" icon={faStar} size="lg" />
+              <h2 className="section-header">FEATURED</h2>
+              <p className="section-description">Check out our featured sauce.</p>
+              <button className="hero-btn">VIEW FEATURED</button>
+            </div>
+
+            <img
+              className="section-img"
+              src="../public/images/LandingPage-Images/bundles.png"
               alt=""
             />
           </div>
