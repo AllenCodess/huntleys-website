50565bc456388ac64bd528e9b36ee44cf43c7b72
AllenCodess <Aph296@protonmail.com>
2026-05-29 00:29:57 -0400

    added img above footer and gave it a fixed height. might need to fix later

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -10,6 +10,15 @@ const HomeScreen = () => {
       <HeroSection />
       <FirstSection />
       <SecondSection />
+      <section className="third-section third-section-container">
+        <div className="img-container-third-section">
+          <img
+            className="third-section-img"
+            src="public/images/LandingPage-Images/above-footer-img.png"
+            alt=""
+          />
+        </div>
+      </section>
     </>
   );
 };
