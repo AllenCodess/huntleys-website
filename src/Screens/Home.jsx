c04918a96ecd8df5b2958f96965c9035a93ab4bf
AllenCodess <Aph296@protonmail.com>
2026-05-29 14:08:21 -0400

    added Thirs Section component and remove contents from homescreen

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import FirstSection from "../components/FirstSection";
 import SecondSection from "../components/SecondSection";
+import ThirdSection from "../components/ThirdSection";
 
 const HomeScreen = () => {
   return (
@@ -10,19 +11,8 @@ const HomeScreen = () => {
       <HeroSection />
       <FirstSection />
       <SecondSection />
-      <section className="third-section third-section-container">
-        <div className="img-container-third-section">
-          <img
-            className="third-section-img"
-            src="public/images/LandingPage-Images/above-footer-img.png"
-            alt=""
-          />
-          <div className="third-section-header">
-            <h2 className="third-section-title-header">Made for Every Meal</h2>
-            <h2 className="third-section-subheader">FROM SEAFOOD TO BBQ WE MAKE IT BETTER.</h2>
-          </div>
-        </div>
-      </section>
+      <ThirdSection />
+      <footer></footer>
     </>
   );
 };
---
+++ src/components/ThirdSection.jsx
@@ -0,0 +1,19 @@
+const ThirdSection = () => {
+  return (
+    <section className="third-section third-section-container">
+      <div className="img-container-third-section">
+        <img
+          className="third-section-img"
+          src="public/images/LandingPage-Images/above-footer-img.png"
+          alt=""
+        />
+        <div className="third-section-header">
+          <h2 className="third-section-title-header">Made for Every Meal</h2>
+          <h2 className="third-section-subheader">FROM SEAFOOD TO BBQ WE MAKE IT BETTER.</h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ThirdSection;
