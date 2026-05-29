5857142137082b81557a01b580a3ee568e8b6efc
AllenCodess <Aph296@protonmail.com>
2026-05-29 15:01:13 -0400

    gave footer its own component

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -3,6 +3,7 @@ import HeroSection from "../components/HeroSection";
 import FirstSection from "../components/FirstSection";
 import SecondSection from "../components/SecondSection";
 import ThirdSection from "../components/ThirdSection";
+import Footer from "../components/Footer";
 
 const HomeScreen = () => {
   return (
@@ -12,13 +13,7 @@ const HomeScreen = () => {
       <FirstSection />
       <SecondSection />
       <ThirdSection />
-      <footer>
-        <div className="footer-container container">
-          <h1 className="logo-name">Huntley's</h1>
-          <h4 className="secondary-name">SAUCES</h4>
-          <h5 className="footer-description"> Makes all food complete.</h5>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container container">
+        <div className="footer-logo">
+          <h1 className="logo-name">Huntley's</h1>
+          <h4 className="secondary-name">SAUCES</h4>
+          <h5 className="footer-description"> Makes all food complete.</h5>
+        </div>
+        <div className="footer-socials">
+          <h3 className="footer-header">Follow Us</h3>
+          <Link to={"https://www.instagram.com/huntleysinc/"}>
+            <FontAwesomeIcon icon={faInstagram} size="xl" />
+          </Link>
+          <Link
+            to={"https://www.facebook.com/people/Huntleys-Seafood-Dippin-Sauce/100057752495962/#"}
+          >
+            <FontAwesomeIcon icon={faFacebookSquare} size="xl" />
+          </Link>
+        </div>
+        <div className="footer-shop">
+          <h3 className="footer-header">SHOP</h3>
+          <Link className="footer-links" to={"/products"}>
+            ALL PRODUCTS
+          </Link>
+        </div>
+        <div className="footer-company">
+          <h3 className="footer-header">COMPANY</h3>
+          <Link className="footer-links" to={"/about"}>
+            ABOUT US
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
