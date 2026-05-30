96b6feeba430df3fafc3f34cb6a98086b90547e8
AllenCodess <Aph296@protonmail.com>
2026-05-29 21:47:44 -0400

    added the profile image to the right side of the about hero section

--- src/Screens/About.jsx
+++ src/Screens/About.jsx
@@ -9,8 +9,21 @@ const About = () => {
           <img
             className="about-hero-img"
             src="/images/LandingPage-Images/Jars/6JarsBackground.png"
-            alt=""
+            alt="Six Huntley Products"
           />
+          <div className="about-header-container">
+            <h2 className="about-main-header">Our Story</h2>
+            <h1 className="about-sub-header">
+              MADE WITH PURPOSE INSPIRED BY <strong>FAMILY.</strong>
+            </h1>
+          </div>
+          <div className="right-side-hero-container">
+            <img
+              className="about-hero-img-tiffany"
+              src="/images/LandingPage-Images/Tiffany3RemovebgPreview.png"
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </>
