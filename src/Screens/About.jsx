a2436ce87b40f3ea091deaafc86d3f4fe14504fa
AllenCodess <Aph296@protonmail.com>
2026-05-30 18:30:06 -0400

    kitchen to table section now has its own component

--- src/Screens/About.jsx
+++ src/Screens/About.jsx
@@ -1,6 +1,8 @@
 import AboutHeroSection from "../components/AboutHeroSection";
 import AboutMeetTiifany from "../components/AboutMeetTiffany";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import KitchentoTableSection from "../components/KitchentoTableSection";
 
 const About = () => {
   return (
@@ -8,30 +10,8 @@ const About = () => {
       <Header />
       <AboutHeroSection />
       <AboutMeetTiifany />
-      <div className="kitchen-to-table-section-container container">
-        <img
-          className="kitchen-to-table-img"
-          src="/images/LandingPage-Images/Jars/3saucesBackground.png"
-          alt=""
-        />
-
-        <div className="kitchen-to-table-text-container">
-          <h2 className="from-kitchen-header">From a Kitchen to Your Table</h2>
-          <p className="from-kitchen-description">
-            Tiffany K. Huntley is a true example of a successful businesswoman and seafood
-            enthusiast. With more than 25 years of federal service, where she currently holds a
-            management position, Tiffany recognized a need for products that deliver exceptional
-            value, high quality, and delicious taste. In 1995, she established Huntley's Inc. in her
-            own kitchen. Since then, she has traveled throughout the United States promoting her
-            products. Today, Tiffany's products can be found in several major grocery stores and
-            restaurants, including Wegmans Food Markets, Whole Foods Market, Giant Food, Edwards
-            Seafood, Cameron's Seafood Markets, and Department of Defense facilities in Kuwait, just
-            to name a few. Tiffany strongly believes in and lives by a quote from Abraham Lincoln:
-            "Good things may come to those who wait, but only the things left by those who hustle."
-          </p>
-          <p className="tgbtg">tGbtg!</p>
-        </div>
-      </div>
+      <KitchentoTableSection />
+      <Footer />
     </>
   );
 };
---
+++ src/components/KitchentoTableSection.jsx
@@ -0,0 +1,45 @@
+const KitchentoTableSection = () => {
+  return (
+    <>
+      <div className="kitchen-to-table-section-container container">
+        <img
+          className="kitchen-to-table-img"
+          src="/images/LandingPage-Images/Jars/3saucesBackground.png"
+          alt=""
+        />
+
+        <div className="kitchen-to-table-text-container">
+          <h2 className="from-kitchen-header">From a Kitchen to Your Table</h2>
+          <p className="from-kitchen-description">
+            Tiffany K. Huntley is a true example of a successful businesswoman and seafood
+            enthusiast. With more than 25 years of federal service, where she currently holds a
+            management position, Tiffany recognized a need for products that deliver exceptional
+            value, high quality, and delicious taste. In 1995, she established Huntley's Inc. in her
+            own kitchen. Since then, she has traveled throughout the United States promoting her
+            products. Today, Tiffany's products can be found in several major grocery stores and
+            restaurants, including Wegmans Food Markets, Whole Foods Market, Giant Food, Edwards
+            Seafood, Cameron's Seafood Markets, and Department of Defense facilities in Kuwait, just
+            to name a few. Tiffany strongly believes in and lives by a quote from Abraham Lincoln:
+            "Good things may come to those who wait, but only the things left by those who hustle."
+          </p>
+          <p className="tgbtg">tGbtg!</p>
+        </div>
+      </div>
+      <section className="third-section third-section-container">
+        <div className="img-container-third-section">
+          <img
+            className="third-section-img"
+            src="/images/LandingPage-Images/abovefooterabout.png"
+            alt=""
+          />
+          <div className="third-section-header">
+            <h2 className="third-section-title-header">Taste the Difference</h2>
+            <h2 className="third-section-subheader">MAKES ALL FOOD COMPLETE!</h2>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default KitchentoTableSection;
