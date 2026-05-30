ee926bfccf902df98163818f987f171d95ccbe18
AllenCodess <Aph296@protonmail.com>
2026-05-30 14:03:09 -0400

    started the structure of the meet tiffany text section

--- src/Screens/About.jsx
+++ src/Screens/About.jsx
@@ -6,8 +6,27 @@ const About = () => {
     <>
       <Header />
       <AboutHeroSection />
-      <div className="meet-tiffany-section">
-        <h2>MEET TIFFANY K. HUNTLEY</h2>
+      <div className="meet-tiffany-section container">
+        <div className="meet-tiffany-text-container">
+          <h2 className="meet-tiffany-header">MEET TIFFANY K. HUNTLEY</h2>
+          <p className="meet-tiffany-description">
+            Tiffany K. Huntley currently resides in Largo, Maryland. She is humble and grateful that
+            God has entrusted her with a condiment company. Tiffany takes no credit for producing
+            her high-quality gourmet products, including Huntley's Seafood Dippin' Sauce (All
+            Natural & Original Recipe), Huntley's Spicy Tartar Sauce, Huntley's Sweet & Smokey
+            Sauce, and Huntley's Cocktail Sauce. She has many more recipes in development and is
+            excited to bring them to grocery stores very soon.
+          </p>
+          <p className="meet-tiffany-description2">
+            Tiffany K. Huntley is a child of God and the proud mother of three beautiful children.
+            She works diligently to ensure that every area of her life remains centered and
+            balanced. Tiffany deeply values her family and friends and is grateful for their
+            constant encouragement, strength, and support. Their contributions have helped her
+            pursue and fulfill the vision that God has placed on her heart.
+          </p>
+          <p className="tgbtg">tGbtg</p>
+        </div>
+        <div className="meet-tiffany-img-container"></div>
       </div>
     </>
   );
