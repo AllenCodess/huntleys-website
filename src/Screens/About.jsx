c98924d203197d1063a12cfbe07f830df86d6396
AllenCodess <Aph296@protonmail.com>
2026-05-29 21:26:28 -0400

    added teh about section and some images to use with it

--- src/Screens/About.jsx
+++ src/Screens/About.jsx
@@ -4,6 +4,15 @@ const About = () => {
   return (
     <>
       <Header />
+      <div className="AboutHeroSection container">
+        <div className="about-hero-container">
+          <img
+            className="about-hero-img"
+            src="/images/LandingPage-Images/Jars/6JarsBackground.png"
+            alt=""
+          />
+        </div>
+      </div>
     </>
   );
 };
