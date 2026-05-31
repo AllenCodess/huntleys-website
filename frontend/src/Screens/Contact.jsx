cd1ac4c8318d8dd04df6cb04728ac6aef2288624
AllenCodess <Aph296@protonmail.com>
2026-05-31 19:06:39 -0400

    working on form section

--- frontend/src/Screens/Contact.jsx
+++ frontend/src/Screens/Contact.jsx
@@ -1,25 +1,13 @@
 import Header from "../components/Header";
+import ContactHeroSection from "../components/ContactHeroSection";
+import ContactForm from "../components/ContactForm";
 
 const ContactScreen = () => {
   return (
     <>
       <Header />
-      <div className="contact-hero-container container">
-        <div className="img-container">
-          <img className="contact-hero-img" src="/images/aboutheroimg.png" alt="" />
-
-          <div className="contact-hero-section-text-container">
-            <h2 className="contact-hero-title">We'd Love to Hear From You!</h2>
-            <h2 className="contact-hero-header">
-              LET'S STAY <span className="contact-hero-connected">CONNECTED.</span>
-            </h2>
-            <p className="contact-hero-description">
-              Have a question about our sauces, need help with an order, or interested in partnering
-              with us? We're here to help!
-            </p>
-          </div>
-        </div>
-      </div>
+      <ContactHeroSection />
+      <ContactForm />
     </>
   );
 };
---
+++ frontend/src/components/ContactForm.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+const ContactForm = () => {
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [subject, setSubject] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  return (
+    <>
+      <div className="contact-form-container container">
+        <h2 className="contact-form-title">Send Us A message</h2>
+        <h2 className="contact-form-header">Contact US</h2>
+        <div className="contact-inputs">
+          <div className="name-inputs">
+            <label>First Name</label>
+            <input
+              type="text"
+              name="firstname"
+              value={firstname}
+              placeholder="First Name"
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <label>Last Name</label>
+            <input
+              type="text"
+              name="lastname"
+              value={lastname}
+              placeholder="Last Name"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <label>Email</label>
+          <input
+            className="input-field"
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label>Phone Number</label>
+          <input
+            className="input-field"
+            type="text"
+            name="phoneNumber"
+            value={phoneNumber}
+            placeholder="Phone Number"
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+          <label>Subject</label>
+          <input
+            className="input-field"
+            type="text"
+            name="Subject"
+            value={subject}
+            placeholder="Subject"
+            onChange={(e) => setSubject(e.target.value)}
+          />
+          <label>Message</label>
+          <textarea
+            className="input-field"
+            name="message"
+            value={message}
+            placeholder="Your Message"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactForm;
