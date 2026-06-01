6ba999917253962c36db8a90f1c918e96d20b049
AllenCodess <Aph296@protonmail.com>
2026-06-01 00:52:39 -0400

    hardcoded form is complete until i start the backend

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -4,76 +4,92 @@ import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const ContactForm = () => {
-  const [firstname, setFirstName] = useState("");
-  const [lastname, setLastName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [subject, setSubject] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    phoneNumber: "",
+    subject: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <>
       <div className="contact-flex-container container">
         <div className="contact-form-container ">
           <h2 className="contact-form-title">Send Us A message</h2>
           <h2 className="contact-form-header">CONTACT US</h2>
-          <div className="contact-inputs">
-            <div className="name-inputs">
-              <label>First Name</label>
+          <form onSubmit={handleSubmit}>
+            <div className="contact-inputs">
+              <div className="name-inputs">
+                <label>First Name</label>
+                <input
+                  className="name-input-field"
+                  type="text"
+                  name="firstname"
+                  value={formData.firstname}
+                  placeholder="First Name"
+                  onChange={handleChange}
+                />
+                <label>Last Name</label>
+                <input
+                  className="name-input-field"
+                  type="text"
+                  name="lastname"
+                  value={formData.lastname}
+                  placeholder="Last Name"
+                  onChange={handleChange}
+                />
+              </div>
+              <label>Email</label>
               <input
-                className="name-input-field"
-                type="text"
-                name="firstname"
-                value={firstname}
-                placeholder="First Name"
-                onChange={(e) => setFirstName(e.target.value)}
+                className="input-field"
+                type="email"
+                name="email"
+                value={formData.email}
+                placeholder="Email"
+                onChange={handleChange}
               />
-              <label>Last Name</label>
+              <label>Phone Number</label>
               <input
-                className="name-input-field"
+                className="input-field"
                 type="text"
-                name="lastname"
-                value={lastname}
-                placeholder="Last Name"
-                onChange={(e) => setLastName(e.target.value)}
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                placeholder="Phone Number"
+                onChange={handleChange}
               />
+              <label>Subject</label>
+              <input
+                className="input-field"
+                type="text"
+                name="subject"
+                value={formData.subject}
+                placeholder="Subject"
+                onChange={handleChange}
+              />
+              <label>Message</label>
+              <textarea
+                className="input-field"
+                name="message"
+                value={formData.message}
+                placeholder="Your Message"
+                onChange={handleChange}
+              />
+              <button type="submit" className="contact-form-btn">
+                SEND MESSAGE
+              </button>
             </div>
-            <label>Email</label>
-            <input
-              className="input-field"
-              type="email"
-              name="email"
-              value={email}
-              placeholder="Email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <label>Phone Number</label>
-            <input
-              className="input-field"
-              type="text"
-              name="phoneNumber"
-              value={phoneNumber}
-              placeholder="Phone Number"
-              onChange={(e) => setPhoneNumber(e.target.value)}
-            />
-            <label>Subject</label>
-            <input
-              className="input-field"
-              type="text"
-              name="Subject"
-              value={subject}
-              placeholder="Subject"
-              onChange={(e) => setSubject(e.target.value)}
-            />
-            <label>Message</label>
-            <textarea
-              className="input-field"
-              name="message"
-              value={message}
-              placeholder="Your Message"
-              onChange={(e) => setMessage(e.target.value)}
-            />
-            <button className="contact-form-btn">SEND MESSAGE</button>
-          </div>
+          </form>
         </div>
         <div className="get-in-touch ">
           <h2 className="contact-form-title">We're Here For You</h2>
