11130a32266accec240bf7cf0fb2b4864e132d8f
AllenCodess <Aph296@protonmail.com>
2026-06-04 06:56:09 -0400

    edited the products.js file and started the project model

--- backend/data/products.js
+++ backend/data/products.js
@@ -4,6 +4,9 @@ const products = [
     name: "Seafood Dippin Sauce",
     image: "/images/seafoodDippinV2.png",
     price: 6.99,
+    countInStock: 12,
+    rating: 5,
+    numReviews: 15,
     description:
       "Enhance every seafood dish with Huntley's Seafood Dippin' Sauce. Perfect for crab, shrimp, lobster, fish, vegetables, and poultry. Also great for crab cakes, seafood salads, deviled eggs, tuna sandwiches, shrimp tacos, and fish tacos.",
   },
@@ -12,6 +15,9 @@ const products = [
     name: "CockTail Sauce",
     image: "/images/cockTailSauce.png",
     price: 3.99,
+    countInStock: 12,
+    rating: 5,
+    numReviews: 15,
     description:
       "Not your average cocktail sauce. Huntley's Cocktail Sauce delivers the perfect balance of zesty spice and subtle sweetness, making it an ideal companion for shrimp, crab cakes, and all your favorite seafood dishes.",
   },
@@ -20,6 +26,9 @@ const products = [
     name: "Tartar Sauce",
     image: "/images/TartarSauce.png",
     price: 5.99,
+    countInStock: 12,
+    rating: 5,
+    numReviews: 15,
     description:
       "Made from Grandma Hayes' original recipe, Huntley's Spicy Tartar Sauce delivers the perfect balance of bold spice and classic tartar flavor. Delicious with fried fish, shrimp, crab cakes, and fish tacos, it's the only seafood sauce you'll need.",
   },
@@ -28,6 +37,9 @@ const products = [
     name: "BBQ and Honey Mustard",
     image: "/images/BBQandHoneyMustard.png",
     price: 4.99,
+    countInStock: 12,
+    rating: 5,
+    numReviews: 15,
     description:
       "Huntley's BBQ & Honey Mustard Sauce combines rich BBQ flavor with the perfect touch of sweet honey mustard. Whether you're dipping chicken tenders, wings, fries, or spreading it on sandwiches and burgers, this bold and flavorful sauce makes every bite more delicious.",
   },
@@ -36,6 +48,9 @@ const products = [
     name: "Blazin Blue Cheese",
     image: "/images/BlazinBlueCheese.png",
     price: 4.99,
+    countInStock: 12,
+    rating: 5,
+    numReviews: 15,
     description:
       "Huntley's Blazin Blue Cheese Sauce delivers the rich, creamy flavor of classic blue cheese with a bold kick of heat. Perfect for wings, burgers, sandwiches, wraps, and fresh vegetables, this versatile sauce adds the perfect balance of spice and flavor to every bite.",
   },
@@ -44,6 +59,9 @@ const products = [
     name: "Blazin Ranch",
     image: "/images/BlazinRanch.png",
     price: 4.99,
+    countInStock: 12,
+    rating: 5,
+    numReviews: 15,
     description:
       "Huntley's Blazin' Ranch Sauce delivers the creamy, classic flavor of ranch with a bold kick of heat. Perfect for wings, chicken tenders, burgers, sandwiches, wraps, fries, and fresh vegetables, this versatile sauce adds the perfect balance of cool, creamy flavor and zesty spice to every bite.",
   },
