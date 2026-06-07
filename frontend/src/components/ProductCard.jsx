65519180781625e93a9017300c221002fb635983
AllenCodess <Aph296@protonmail.com>
2026-06-07 15:56:29 -0400

    the add to cart buttons work on the products page

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,6 +1,15 @@
 import { Link } from "react-router";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { addToCart } from "../slices/cartSlice.js";
 
 const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  const [qty, setQty] = useState(1);
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...product, qty }));
+  };
+
   return (
     <div className="card-container">
       <Link className="productslink" to={`/product/${product._id}`}>
@@ -12,7 +21,13 @@ const ProductCard = ({ product }) => {
           <p className="product-title">{product.name}</p>
         </Link>
         <p className="product-price">{product.price}</p>
-        <button className="product-btn">ADD TO CART</button>
+        <button
+          className="product-btn"
+          onClick={addToCartHandler}
+          disabled={product.countInStock === 0}
+        >
+          ADD TO CART
+        </button>
       </div>
     </div>
   );
