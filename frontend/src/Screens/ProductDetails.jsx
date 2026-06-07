c1bda03b136c1b9594296eaafd09141d8fb20864
AllenCodess <Aph296@protonmail.com>
2026-06-06 22:48:37 -0400

    fetching data with rtk instead of useEffect

--- frontend/src/Screens/ProductDetails.jsx
+++ frontend/src/Screens/ProductDetails.jsx
@@ -1,39 +1,33 @@
 import { useParams } from "react-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { useEffect, useState } from "react";
+import { useGetProductDetailsQuery } from "../slices/productApiSlice.js";
 
 const ProductDetails = () => {
   const { id: productId } = useParams();
-
-  const [product, setProduct] = useState({});
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetch(`/api/products/${productId}`);
-      const response = await data.json();
-
-      setProduct(response);
-    };
-
-    fetchData();
-  }, [productId]);
+  const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
 
   return (
     <>
       <Header />
-      <div className="product-details-container ">
-        <div className="product-details-left">
-          <img className="productDetailsimg" src={product.image} alt="" />
+      {isLoading ? (
+        <div> Loading . . .</div>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <div className="product-details-container ">
+          <div className="product-details-left">
+            <img className="productDetailsimg" src={product.image} alt="" />
+          </div>
+          <div className="product-details-right">
+            <h1 className="huntleys-heading">HUNTLEY'S</h1>
+            <h2 className="product-details-sauce-name">{product.name}</h2>
+            <p className="product-details-description">{product.description}</p>
+            <p className="product-details-price">${product.price}</p>
+            <button className="product-details-btn">ADD TO CART</button>
+          </div>
         </div>
-        <div className="product-details-right">
-          <h1 className="huntleys-heading">HUNTLEY'S</h1>
-          <h2 className="product-details-sauce-name">{product.name}</h2>
-          <p className="product-details-description">{product.description}</p>
-          <p className="product-details-price">${product.price}</p>
-          <button className="product-details-btn">ADD TO CART</button>
-        </div>
-      </div>
+      )}
       <Footer />
     </>
   );
