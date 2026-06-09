cddef5090f37e21a3298ce15e231039fbf514b13
AllenCodess <Aph296@protonmail.com>
2026-06-09 09:20:16 -0400

    establishing protected routes adn admin routes via jwt/ auth middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import Product from "./models/productModel.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
