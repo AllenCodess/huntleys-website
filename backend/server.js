8679141968ef47affc7db4a74ef9be2c0bd458f7
AllenCodess <Aph296@protonmail.com>
2026-06-08 20:25:07 -0400

    user routes and user controller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import Product from "./models/productModel.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -12,13 +13,18 @@ const port = process.env.PORT;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/products", productRoutes);
-app.use(notFound);
-app.use(errorHandler);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
