26f2f9a8f4f83f9a632b411cdb4d607b6531bb2a
AllenCodess <Aph296@protonmail.com>
2026-06-10 15:19:26 -0400

    login screen and logic

--- frontend/src/Screens/Login.jsx
+++ frontend/src/Screens/Login.jsx
@@ -1,19 +1,43 @@
 import Header from "../components/Header";
+import { useState } from "react";
 
 const LoginScreen = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <>
       <Header />
       <div className="login-page-container container">
         <div className="login-form-container">
-          <h1 className="login-header">LOGIN</h1>
-          <form>
-            <input className="login-inputs" type="email" name="email" placeholder="Email" />
+          <h1 className="login-header">SIGN IN</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="login-inputs"
+              type="email"
+              name="email"
+              value={formData.email}
+              placeholder="Email"
+              onChange={handleChange}
+            />
             <input
               className="login-inputs"
               type="password"
               name="password"
               placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
             />
             <button className="login-btn">Login</button>
           </form>
