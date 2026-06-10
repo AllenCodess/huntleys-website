2be8f7073c648354d565de7d23dd9410fbc005fc
AllenCodess <Aph296@protonmail.com>
2026-06-09 20:50:09 -0400

    creating the login screen

--- frontend/src/Screens/Login.jsx
+++ frontend/src/Screens/Login.jsx
@@ -4,6 +4,21 @@ const LoginScreen = () => {
   return (
     <>
       <Header />
+      <div className="login-page-container container">
+        <div className="login-form-container">
+          <h1 className="login-header">LOGIN</h1>
+          <form>
+            <input className="login-inputs" type="email" name="email" placeholder="Email" />
+            <input
+              className="login-inputs"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+            <button className="login-btn">Login</button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
