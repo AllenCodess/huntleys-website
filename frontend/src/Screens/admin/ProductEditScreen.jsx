f688007f857eeeda6ed47252cbe761a289838d4e
AllenCodess <Aph296@protonmail.com>
2026-06-15 16:02:15 -0400

    css and housekeeping

--- frontend/src/Screens/admin/ProductEditScreen.jsx
+++ frontend/src/Screens/admin/ProductEditScreen.jsx
@@ -12,8 +12,6 @@ const ProductEditScreen = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
   const [image, setImage] = useState("");
-  const [brand, setBrand] = useState("");
-  const [category, setCategory] = useState("");
   const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState("");
 
