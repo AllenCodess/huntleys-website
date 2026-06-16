cfb828bdaa52d7e765863f718d8b2f406b701da2
AllenCodess <Aph296@protonmail.com>
2026-06-16 00:11:40 -0400

    Fix wildcard route path order

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use("/api/contact", contactRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*/splat", (req, res) =>
+  app.get("/*splat", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")),
   );
 } else {
