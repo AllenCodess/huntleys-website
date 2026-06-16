4a76f8e384a43d733371f0c34ce8bd584143eb1c
AllenCodess <Aph296@protonmail.com>
2026-06-15 22:59:52 -0400

    added changes to the upload image

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,26 +1,30 @@
 import path from "path";
 import express from "express";
 import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename(req, file, cb) {
-    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "huntleys-sauces",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
   },
 });
 
 function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png/;
+  const filetypes = /jpe?g|png|webp/;
+  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
+  const mimetype = mimetypes.test(file.mimetype);
 
   if (extname && mimetype) {
     return cb(null, true);
   } else {
-    cb({ message: "Images only!" });
+    cb(new Error("Images only!"), false);
   }
 }
 
@@ -32,9 +36,9 @@ const upload = multer({
 });
 
 router.post("/", upload.single("image"), (req, res) => {
-  res.send({
+  res.status(200).send({
     message: "Image uploaded successfully",
-    image: `/${req.file.path}`,
+    image: req.file.path,
   });
 });
 
